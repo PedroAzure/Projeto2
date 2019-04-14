931a42951a0f35e234471afaffb264d4be24a41a
PedroAzure <49622290+PedroAzure@users.noreply.github.com>
2019-04-14 19:49:38 -0300

    Movimento Vertical

---
+++ Projeto2/Assets/PlayerMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float maxSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = transform.position;
+        pos.y += Input.GetAxis("Vertical") * maxSpeed * Time.deltaTime; 
+
+        transform.position = pos;
+    }
+}
