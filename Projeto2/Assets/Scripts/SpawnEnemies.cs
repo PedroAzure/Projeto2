b9a30feaf9a716b07e658fc458e54d31b979fe86
PedroAzure <49622290+PedroAzure@users.noreply.github.com>
2019-04-23 17:15:49 -0300

    Commit para pull

--- Projeto2/Assets/Scripts/SpawnEnemies.cs
+++ Projeto2/Assets/Scripts/SpawnEnemies.cs
@@ -2,17 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpawnEnemies : MonoBehaviour
+public class EnemySpawner : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    public GameObject enemyPrefab;
+
+    public float enemyRate = 5;
+    float nextEnemy;
+
     void Start()
     {
-        
+        nextEnemy = enemyRate;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        nextEnemy -= Time.deltaTime;
+
+        if(nextEnemy <= 0) 
+        {
+            nextEnemy = enemyRate;
+
+            Vector3 position = new Vector3(11, Random.Range(-4.4f, 4.3f), 0);
+          
+            Instantiate(enemyPrefab, position, Quaternion.identity);
+        }        
     }
 }
