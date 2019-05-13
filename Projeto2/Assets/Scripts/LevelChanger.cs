6d0c98da3a07915b1a4cb2a121a41dd105787110
PedroAzure <49622290+PedroAzure@users.noreply.github.com>
2019-05-13 00:19:58 -0300

    Fade e Transição

---
+++ Projeto2/Assets/Scripts/LevelChanger.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelChanger : MonoBehaviour
+{
+    public Animator animator;
+
+    private int levelToLoad;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.touchCount >= 3)
+        {
+            FadeToNextLevel();
+        }
+        
+    }
+
+    public void FadeToLevel(int levelIndex)
+    {
+        levelToLoad = levelIndex;
+        animator.SetTrigger("FadeOut");
+        animator.SetTrigger("Loading");
+    }
+
+    public void FadeToNextLevel()
+    {
+        if(SceneManager.GetActiveScene().buildIndex <= 3)
+            FadeToLevel(SceneManager.GetActiveScene().buildIndex + 1);
+        else
+            FadeToLevel(0);
+        
+    }
+
+    public void OnFadeComplete()
+    {
+        SceneManager.LoadScene(levelToLoad);
+    }
+
+    public void FadeToHome()
+    {
+        levelToLoad = 0;
+        animator.SetTrigger("FadeOut");
+        animator.SetTrigger("Loading");
+    }
+}
